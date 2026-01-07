cad50d5816da199ee7b79365fe46c9bc9b9051a5
lgr-2024 <lgr@devtools.kr>
2026-01-07 13:15:10 +0900

    API Document 작성

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,5 +1,8 @@
 package zerobase.weather.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weather.domain.Diary;
@@ -16,27 +19,32 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @Operation(summary = "날짜와 내용으로 일기 생성")
     @PostMapping("create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
+    @Operation(summary = "선택한 날짜의 일기 조회")
     @GetMapping("read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "선택한 기간의 일기 조회")
     @GetMapping("read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "조회할 기간의 시작일", example = "2025-12-25") LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(description = "조회할 기간의 종료일", example = "2025-12-25") LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @Operation(summary = "선택한 날짜의 일기를 해당 내용으로 일기 수정")
     @PutMapping("/update/diary")
     void updateDairy(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.updateDiary(date, text);
     }
 
+    @Operation(summary = "선택한 날짜의 일기를 해당 내용으로 일기 삭제")
     @DeleteMapping("delete/diary")
     void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         diaryService.deleteDiary(date);
